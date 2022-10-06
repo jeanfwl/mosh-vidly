e2b0888c5654b162da4d0c13f841bb2c2a228208
Jean Paulo Talar <jean.talar@sepac.lan>
2022-10-06 13:49:35 -0300

    Adicionado método para inserir Customer. Melhorado nome das Actions e Views do formulario Customer

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -28,14 +28,39 @@ namespace Vidly.Controllers
             return View(customers);
         }
 
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel {
+
+            var viewModel = new CustomerFormViewModel {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
 
         public ActionResult Details(int id)

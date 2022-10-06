c90d84e5ef15c9f031b0947fab0f240964fd973a
Jean Paulo Talar <jean.talar@sepac.lan>
2022-10-06 08:41:45 -0300

    Adicionado modelo MembershipType e suas migrações no banco

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -7,24 +8,28 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        private List<Customer> GetCustomers()
+        private ApplicationDbContext _context;
+        public CustomersController()
         {
-            return new List<Customer> {
-                new Customer { Id = 1, Name = "Jean" },
-                new Customer { Id = 2, Name = "Zaito" },
-                new Customer { Id = 3, Name = "Carvaio" }
-            };
+            _context = new ApplicationDbContext();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers
+                                .Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers()
+            var customer = _context.Customers
                             .FirstOrDefault(c => c.Id == id);
 
             if (customer == null)

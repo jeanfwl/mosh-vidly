5d888c546da4ed4cf09eed1748ff6b8642bd91d7
Jean Paulo Talar <jean.talar@sepac.lan>
2022-10-07 07:39:42 -0300

    Adicionada validação customizada para Birthdate de Customer

---
+++ Models/Min18YearsIfAMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 ||
+                customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (!customer.Birthdate.HasValue)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Now.Year - customer.Birthdate.Value.Year;
+
+            return (age < 18)
+                ? new ValidationResult("Customer age must be at least 18 years old to go a membership.")
+                : ValidationResult.Success;
+        }
+    }
+}

a1ee8924ef079fb8751c69c81f3000d6eb55d2a1
Jean Paulo Talar <jeanttalar@gmail.com>
2022-10-10 16:30:16 -0300

    Adicionado configuração para renderizar data table via json da API Customers, removido Model da View Index

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -22,10 +22,7 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            var customers = _context.Customers
-                                .Include(c => c.MembershipType).ToList();
-
-            return View(customers);
+            return View();
         }
 
         [HttpPost]

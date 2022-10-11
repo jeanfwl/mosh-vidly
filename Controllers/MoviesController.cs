5d91a94eca2a773b1102df32abd2192476221d53
Jean Paulo Talar <jeanttalar@gmail.com>
2022-10-11 14:14:24 -0300

    Adicionado autorização para inserir, atualizar e excluir Movies para somente Store Managers, tanto na API quanto Controller

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -50,6 +50,7 @@ namespace Vidly.Controllers
             return View("MovieForm", viewModel);
         }
 
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Edit(int id)
         {
             var movie = _context.Movies

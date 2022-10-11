b59231600ac3e93b6407fbaa8003721c4d2d18ae
Jean Paulo Talar <jeanttalar@gmail.com>
2022-10-11 11:03:20 -0300

    Adicionado View para Movies List sem interações para usuários simples. Configurado cargos em nova classe RoleName.cs

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -22,7 +22,10 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            if (User.IsInRole(RoleName.CanManageMovies))
+                return View("List");
+
+            return View("ReadOnlyList");
         }
 
         public ActionResult Details(int id)
@@ -37,6 +40,7 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult New()
         {
             var viewModel = new MovieFormViewModel {

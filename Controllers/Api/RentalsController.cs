e0ae3c4a78f27e6825bae40d2271d7a90c04bc92
Jean Paulo Talar <jeanttalar@gmail.com>
2022-10-13 09:20:15 -0300

    Adicionado Rentals. Migration, API e Dto

---
+++ Controllers/Api/RentalsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(NewRentalDto newRentalDto)
+        {
+            var customer = _context.Customers.Single(c => c.Id == newRentalDto.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRentalDto.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
